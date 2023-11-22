b886e387ae6ea8d170fb5fb4e2208ba3f1e2cfb8
Igor Luna <igorlunad@gmail.com>
2023-11-22 14:30:56 -0300

    filtros e limpeza 1

--- Filtros.R
+++ Filtros.R
@@ -1,10 +1,32 @@
 library(readr)
-
-
+library(dplyr)
+library(tidyverse)
 
 
 
 # Captura de dados --------------------------------------------------------
 
 EmpresasContabilidade_CNPJ_6920601 <- read_csv("C:/Users/igor-/OneDrive/Área de Trabalho/Deepen/Flamengo/Dados_Contabilidade/EmpresasContabilidade_CNPJ_6920601.csv")
-View(EmpresasContabilidade_CNPJ_6920601)
+
+str(EmpresasContabilidade_CNPJ_6920601)
+
+
+
+# Limpeza básica ----------------------------------------------------------
+
+EmpresasContabilidade_CNPJ_6920601$porte[EmpresasContabilidade_CNPJ_6920601$porte == '01'] <- '1'
+
+# Removendo Nulos
+EmpresasContabilidade_CNPJ_6920601 <- EmpresasContabilidade_CNPJ_6920601[complete.cases(EmpresasContabilidade_CNPJ_6920601$porte), ]
+
+
+#Fazer os filtros de acordo com porte, Existencia de telefone, inicio da atividade e depois localização
+
+x<-EmpresasContabilidade_CNPJ_6920601 %>%
+  dplyr::filter(porte == c('1'))
+
+
+
+
+
+
