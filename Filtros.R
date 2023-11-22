e35c94dd3522f0b2726ad724cb04986be727f2f0
Igor Luna <igorlunad@gmail.com>
2023-11-22 11:46:37 -0300

    Carregando

---
+++ Filtros.R
@@ -0,0 +1,10 @@
+library(readr)
+
+
+
+
+
+# Captura de dados --------------------------------------------------------
+
+EmpresasContabilidade_CNPJ_6920601 <- read_csv("C:/Users/igor-/OneDrive/Área de Trabalho/Deepen/Flamengo/Dados_Contabilidade/EmpresasContabilidade_CNPJ_6920601.csv")
+View(EmpresasContabilidade_CNPJ_6920601)
