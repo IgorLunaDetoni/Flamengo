a37b3428bec3e6c7f2ca834e88624fc385aeaa7e
Igor Luna <igorlunad@gmail.com>
2023-11-23 10:05:21 -0300

    Primeiro filtros

--- Filtros.R
+++ Filtros.R
@@ -20,13 +20,41 @@ EmpresasContabilidade_CNPJ_6920601$porte[EmpresasContabilidade_CNPJ_6920601$port
 EmpresasContabilidade_CNPJ_6920601 <- EmpresasContabilidade_CNPJ_6920601[complete.cases(EmpresasContabilidade_CNPJ_6920601$porte), ]
 
 
+#Tem que ter os números de telefone e email de preferência
+# Removendo NAs de emails -------------------------------------------------
+
+EmpresasContabilidade_CNPJ_6920601 <- EmpresasContabilidade_CNPJ_6920601 %>% filter(!is.na(email))
+
+
 #Fazer os filtros de acordo com porte, Existencia de telefone, inicio da atividade e depois localização
 
 x<-EmpresasContabilidade_CNPJ_6920601 %>%
-  dplyr::filter(porte == c('1'))
+  dplyr::filter(porte == c('3', '5'))
 
 
+# Só números de telefone que fazem sentido
 
+x<-x %>%
+  dplyr::filter(telefone1 != c('-'))
+
+
+# export const portes = [
+#   "Não informado",1
+#   "Micro empresa",2
+#   "Pequeno porte",3
+#   "Mei",4
+#   "Demais",5
+# ];
+# 
+
+# Filtro por cidade -------------------------------------------------------
+
+
+x<-x %>%
+  dplyr::filter(cidade == c('6001'))
+
+
+x %>% write.csv("EmpresasContabilidade3_5_RioCOd6001.csv")
 
 
 
