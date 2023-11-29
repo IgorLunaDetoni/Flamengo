df38d225c1e1b7d876df44edf99634795736cd0b
Igor Luna <igorlunad@gmail.com>
2023-11-29 09:33:34 -0300

    Entrega depois dos bugs que eles causaram >:(

--- Filtros.R
+++ Filtros.R
@@ -50,11 +50,11 @@ x<-x %>%
 
 #Substituindo NULL por NA
 x$razao_social <- ifelse(x$razao_social=='NULL', NA, x$razao_social)
-
+x$email <- ifelse(x$email=='NULL', NA, x$email)
 
 # Se os valores das colunas razao social e nome fantasia forem NA remover a coluna
 x <- x[!(is.na(x$razao_social) & is.na(x$nome_fantasia)), ]
-
+x <- x[complete.cases(x$email),]
 # Filtro por cidade -------------------------------------------------------
 
 
@@ -66,6 +66,10 @@ x <- x[!(is.na(x$razao_social) & is.na(x$nome_fantasia)), ]
 
 
 
+
+
+
+
 # Rankear  ----------------------------------------------------------------
 
 x$rank <- ifelse(x$cidade=='6001'& x$porte %in% c('3','5'), 1,
@@ -74,13 +78,30 @@ x$rank <- ifelse(x$cidade=='6001'& x$porte %in% c('3','5'), 1,
 x<-x %>%
   dplyr::filter(lat != 0)
 
+# Removendo emails duplicados ---------------------------------------------
+
+unicos <- x %>% dplyr::distinct(email,.keep_all = TRUE)
+
+
+# Removendo emails claramente errados -------------------------------------
+
+# List of strings to be removed
+strings_to_remove <- c("0000", "&8203MATHEUSSILVALUAN3@GMAIL.COM", "string3")  # Replace with your strings
+
+
+unicos <- unicos[!(unicos$email %in% strings_to_remove), ]
+
+
 
 # Remover participante de edital, cidade, matriz filial, porte
 
 x<- x %>% select(-c(participante_edital,matriz_filial,cidade, porte))
 x<- x %>% select(-c(cod_nat_juridica))
 
-x %>% write.csv2("EmpresasContabilidade3_5_RioCOd6001.csv", sep = ";")
+unicos %>% write.csv2("EmpresasContabilidade3_5_RioCOd6001.csv", sep = ";")
 
 
+library(openxlsx)
 
+# Save dataframe to an Excel file
+write.xlsx(unicos, file = "EmpresasContabilidade3_5_RioCOd6001.xlsx", rowNames = FALSE)
